b0f9606df36914f7aa4aa94ddddca404db5a6986
Minime998 <noah.fedosoff@gmail.com>
2021-10-11 23:09:44 -0400

    small fix

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -14,12 +14,12 @@ class Solver(object):
 
         for i in range(len(self.board)):
             if i % 3 == 0 and i != 0:
-                print("- - - - - - - - - - - -|")
+                print("- - - - - - - - - - - - | ")
             for y in range(len(self.board[0])):
                 if y % 3 == 0 and y != 0:
                     print(" | ", end="")
                 if y == 8:
-                    print(str(self.board[i][y])+"|")
+                    print(str(self.board[i][y])+" |")
                 else:
                     print(str(self.board[i][y]) + " ", end="")
 
