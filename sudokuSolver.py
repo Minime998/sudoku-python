552c75897b63defa54e36ed210c67bc685b56a66
Minime998 <noah.fedosoff@gmail.com>
2021-10-11 23:07:46 -0400

    made show_board

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -12,4 +12,15 @@ class Solver(object):
             None: N/A
         """
 
+        for i in range(len(self.board)):
+            if i % 3 == 0 and i != 0:
+                print("- - - - - - - - - - - -|")
+            for y in range(len(self.board[0])):
+                if y % 3 == 0 and y != 0:
+                    print(" | ", end="")
+                if y == 8:
+                    print(str(self.board[i][y])+"|")
+                else:
+                    print(str(self.board[i][y]) + " ", end="")
+
         return None
